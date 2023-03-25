6648be5a91b2d094e5f41d80d30bdbc12926adb0
“manvir19” <manvirkaur.kanda@gmail.com>
2023-03-25 12:23:03 +0530

    mm

--- ThapSat/ThapSatWeb/urls.py
+++ ThapSat/ThapSatWeb/urls.py
@@ -3,5 +3,10 @@ from . import views
 urlpatterns = [
     path( '' , views.home , name="home" ),
     path( 'updates' , views.updates , name="updates" ),
+    path( 'departments' , views.departments , name="departments" ),
+    path( 'subsystems' , views.subsystems , name="subsystems" ),
+    path( 'social' , views.social , name="social" ),
+
+
 
 ]
--- ThapSat/ThapSatWeb/views.py
+++ ThapSat/ThapSatWeb/views.py
@@ -5,3 +5,9 @@ def home(request):
     return render(request, 'home.html' ,{})
 def updates(request):
     return render(request, 'updates.html' ,{})
+def departments(request):
+    return render(request, 'departments.html' ,{})
+def subsystems(request):
+    return render(request, 'subsystems.html' ,{})
+def social(request):
+    return render(request, 'social.html' ,{})
