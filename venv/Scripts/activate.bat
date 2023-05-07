beb649c5be8c7063a14c1f23ce891798cafafd8e
anjaanii <amohil_be21@thapar.edu>
2023-05-07 19:14:54 +0530

    AA

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\91896\Desktop\chandann\ThapSat\venv
+set VIRTUAL_ENV=C:\Users\smohi\Desktop\thapsat123\ThapSat\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
